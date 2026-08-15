0f71122baa8166ab3c2e0309c227706ad4f398a3
Mohammad Danish Muttain Khan <md.danishkhan0307@gmail.com>
2026-08-16 01:41:27 +0530

    Add Japanese city names SQL solution

---
+++ EASY/Japanese-City-Names.sql
@@ -0,0 +1,9 @@
+-- Problem: Japanese Cities
+
+-- Question:
+-- Query the names of all the Japanese cities in the CITY table.
+-- The COUNTRYCODE for Japan is JPN.
+
+SELECT name
+FROM CITY
+WHERE countrycode = 'JPN';
