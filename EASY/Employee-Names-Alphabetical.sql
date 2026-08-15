6e95f1036ebe6523e016043b18ce680549d5f7c7
Mohammad Danish Muttain Khan <md.danishkhan0307@gmail.com>
2026-08-16 01:54:38 +0530

    Add employee names alphabetical order solution

---
+++ EASY/Employee-Names-Alphabetical.sql
@@ -0,0 +1,8 @@
+-- Problem: Employee Names - Alphabetical Order
+
+-- Question:
+-- Write a query that prints a list of employee names from the Employee table in alphabetical order.
+
+SELECT name
+FROM employee
+ORDER BY name ASC;
