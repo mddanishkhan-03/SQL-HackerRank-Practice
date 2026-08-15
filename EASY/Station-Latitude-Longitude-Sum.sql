45a623db8456fc9a846ccfbbc5f4e7f053e294fb
Mohammad Danish Muttain Khan <md.danishkhan0307@gmail.com>
2026-08-16 02:20:46 +0530

    Add station latitude longitude sum solution

---
+++ EASY/Station-Latitude-Longitude-Sum.sql
@@ -0,0 +1,10 @@
+-- Problem: Weather Observation Station 2
+
+-- Question:
+-- Query the following two values from the STATION table:
+-- 1. The sum of all values in LAT_N rounded to 2 decimal places.
+-- 2. The sum of all values in LONG_W rounded to 2 decimal places.
+
+SELECT ROUND(SUM(LAT_N), 2),
+       ROUND(SUM(LONG_W), 2)
+FROM STATION;
