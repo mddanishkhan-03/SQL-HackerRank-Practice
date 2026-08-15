20544d4b49649e9cc532f545b59248878b733bca
Mohammad Danish Muttain Khan <md.danishkhan0307@gmail.com>
2026-08-16 02:11:30 +0530

    Add rounded down average population solution

---
+++ EASY/Average-Population-Floor.sql
@@ -0,0 +1,7 @@
+-- Problem: Average Population (Rounded Down)
+
+-- Question:
+-- Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+SELECT FLOOR(AVG(population))
+FROM CITY;
