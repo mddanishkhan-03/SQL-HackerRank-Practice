2562d6d2e8a8359786d3c7745a55a85811afa0dd
Mohammad Danish Muttain Khan <md.danishkhan0307@gmail.com>
2026-08-23 01:41:49 +0530

    Add SQL solution for Challenges problem

---
+++ MEDIUM/Challenges.sql
@@ -0,0 +1,24 @@
+-- Problem: Challenges
+
+-- Question:
+-- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard!
+-- Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge.
+-- Order your output in descending order by the total number of challenges in which the hacker earned a full score.
+-- If more than one hacker received full scores in the same number ofchallenges, then sort them by ascending hacker_id.
+
+SELECT
+    h.hacker_id,
+    h.name
+FROM hackers AS h
+JOIN submissions AS s
+    ON h.hacker_id = s.hacker_id
+JOIN challenges AS c
+    ON s.challenge_id = c.challenge_id
+JOIN difficulty AS d
+    ON c.difficulty_level = d.difficulty_level
+WHERE s.score = d.score
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(s.challenge_id) > 1
+ORDER BY
+    COUNT(s.challenge_id) DESC,
+    h.hacker_id ASC;
